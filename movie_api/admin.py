741ff6612b4efdda39ffac121d314bbef37c6a5c
nelsonchege <68642235+nelsonchege@users.noreply.github.com>
2021-08-11 17:19:55 +0300

    Add files via upload

---
+++ movie_api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Watchlist, StreamingPlatform
+
+# Register your models here.
+admin.site.register(Watchlist)
+admin.site.register(StreamingPlatform)
